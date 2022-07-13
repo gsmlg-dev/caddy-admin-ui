8c6afa3ba5eec53ad671023a6202f4ec263cbbc1
Jonathan Gao <gsmlg.com@gmail.com>
2022-07-13 08:02:50 +0800

    chore: Fix deploy.

--- web/src/UpstreamCard.js
+++ web/src/UpstreamCard.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function UpstreamCard({
